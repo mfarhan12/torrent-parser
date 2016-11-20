abe03f0deb2c492ff39aec46f279d4570214e48f
Mohammad Farhan <mfarhan102@gmail.com>
2016-11-20 10:33:38 -0500

    modified example, added sample torrent files

--- bt_parser/examples/simple_read.py
+++ bt_parser/examples/simple_read.py
@@ -1,12 +1,13 @@
 #pylint: skip-file
 from StringIO import StringIO
 
-file_name = '/home/mohammad/torrent-parser/bt_parser/examples/Elephants Dream (avi) (1024x576).torrent'
-file_name2 = '/home/mohammad/torrent-parser/bt_parser/examples/Honest Man- The Life of R. Budd Dwyer (720p).torrent'
-file_name3 = '/home/mohammad/torrent-parser/bt_parser/examples/sample.torrent'
+#TODO: USE os.cwd to retrieve directory
+
+file_name = '/home/mohammad/torrent-parser/bt_parser/examples/sample.torrent'
 from bt_parser.TorrentParser import TorrentParser 
 
 
-bt_parse = TorrentParser(file_name3)
+bt_parse = TorrentParser(file_name)
 
-# bt_parse = TorrentParser(file_name)
+print bt_parse.get_files()
+
