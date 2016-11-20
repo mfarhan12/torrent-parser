4766dca4e1ace4b018a5ddd0b8eadc748dab64cf
Mohammad Farhan <mfarhan102@gmail.com>
2016-11-20 07:34:09 -0500

    fixed some styling, added comments

--- bt_parser/TorrentParser.py
+++ bt_parser/TorrentParser.py
@@ -1,14 +1,18 @@
 
-import time
 from StringIO import StringIO
 
-class TorrentParser():
+class TorrentParser(object):
     """
     Object that is used to parse torrent files
 
     Can be initialized with bit torrent file name, or passed to open_file
+    This class uses protocols defined by the Bittorrent Protocol Specification v1.0 
+    (https://wiki.theory.org/BitTorrentSpecification)
 
     """
+    _info_dict = {}
+    _raw_binary = []
+
     def __init__(self, file_name=None):
 
         if file_name is None:
@@ -17,88 +21,115 @@ class TorrentParser():
             self.open_file(file_name)
 
     def open_file(self, file_name):
+        """
+        Open a bit torrent file, and parse the contents
 
+        :param file_name: string containing the file name, note this should include the directory
+        :type cmd: str
+        :returns: None
+        """
         self._file_name = file_name
         # read out the initial 'd' char
-        with open(self._file_name, 'rb') as file:
-            self._raw_text = StringIO(file.read()).getvalue()
+        with open(self._file_name, 'rb') as torrent_file:
+            self._raw_binary = StringIO(torrent_file.read()).getvalue()
 
         self._parse_torrent()
+        print self._info_dict
 
     def _parse_torrent(self):
 
-        # remove the initial d
+        # remove the initial dictionary indicatory, and begin parsing info as dictionary
         char = self._pop_raw_string(1)
         if char == 'd':
             self._info_dict = self._decode_dict()
 
-        print self._info_dict
+    def _pop_raw_string(self, num_pop=1):
+        
+        # retrieve the value of the required string
+        popped_string = self._raw_binary[:num_pop]
 
-    def _pop_raw_string(self, n=1):
-        popped_string = self._raw_text[:n]
-        self._raw_text = self._raw_text[n:]
+        # pop the string from the raw data
+        self._raw_binary = self._raw_binary[num_pop:]
         return popped_string
 
     def _read_next_item(self):
-        size_char = ''
-        val = None
 
-        while len(self._raw_text) > 0:
+        # string used to hold the size of the next item
+        size_char = ''
+
+        while len(self._raw_binary) > 0:
             char = self._pop_raw_string()
-            if len(self._raw_text) == 0:
+
+            # return none if we reached end of file
+            if len(self._raw_binary) == 0:
                 return None
+
+            # begin parsing dict, if next item is a dict
             if char == 'd':
                 return self._decode_dict()
-            
+
+            # begin parsing int, if next item is an int
             if char == 'i':
-               return self._decode_integer()
+                return self._decode_integer()
+
+            # return e to indicate an end to the current item being parsed
             if char == 'e':
                 return 'e'
-            
+            # begin parsing list
             if char == 'l':
                 return self._decode_list()
+
+            # colon indicates the current item has ended
             elif char == ':':
-                    break
-            
+                break
+
             else:
                 size_char += char
         if size_char == '':
             return None
+        # return the item, if the item was a string
         return self._pop_raw_string(int(size_char))
-    
+
     def _decode_integer(self):
         val_char = ''
-        while len(self._raw_text) > 0:
+        # keep reading values
+        while len(self._raw_binary) > 0:
             char = self._pop_raw_string()
+
+            # if e is reached, integer has ended, return the integer value
             if char == 'e':
-                return val_char
+                return int(val_char)
             else:
                 val_char += char
-    
+
     def _decode_dict(self):
         original_dict = {}
-        while len(self._raw_text) > 0:
-            
+        while len(self._raw_binary) > 0:
+
             key = self._read_next_item()
 
-            # reached end of file, or end of dict
-            if key == None or key == 'e':
+            # reached end of dict
+            if key is None or key == 'e':
                 return original_dict
 
             val = self._read_next_item()
 
-            original_dict[key] = val
+            #TODO: REMOVE BEFORE RELEASE
+            if key == 'pieces':
+                original_dict[key] = 2
+            else:
+                original_dict[key] = val
 
         return original_dict
 
     def _decode_list(self):
         original_list = []
-        while len(self._raw_text) > 0:
-            
+        while len(self._raw_binary) > 0:
+
             item = self._read_next_item()
 
             # reached end of file, or end of dict
-            if item == None or item == 'e':
+            if item is None or item == 'e':
                 return original_list
 
             if item == 'd':
@@ -107,5 +138,3 @@ class TorrentParser():
 
             original_list.append(item)
         return original_list
-            
-    
